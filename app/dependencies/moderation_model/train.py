0e85d6e29275a0932a350c62c3487e352ec49e61
Gökhan Gerdan <ggerdan@bimser.com.tr>
2024-08-22 13:47:43 +0300

    revision

--- app/dependencies/moderation_model/train.py
+++ app/dependencies/moderation_model/train.py
@@ -11,12 +11,10 @@ from sklearn.model_selection import train_test_split
 
 import matplotlib.pyplot as plt
 
-from tensorflow.keras import regularizers
-from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 df = pd.read_json('app/dependencies/moderation_model/data.jsonl')
 
-with open('/Users/sualpgokalp/Desktop/bimser-staj-2024-2/app/dependencies/moderation_model/embeddings.npy', 'rb') as f:
+with open('app/dependencies/moderation_model/embeddings.npy', 'rb') as f:
     X = np.load(f)
 
 y = df.label.values
@@ -24,26 +22,23 @@ y = df.label.values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(128, activation='relu', input_shape=(X_train.shape[1],),
-                          kernel_regularizer=regularizers.l2(0.001)),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.Dropout(0.4),
+    tf.keras.layers.Dense(128, activation='relu', input_shape=(X_train.shape[1],),),
+    tf.keras.layers.Dropout(0.5),
     
-    tf.keras.layers.Dense(64, activation='relu', kernel_regularizer=regularizers.l2(0.001)),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
     
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), 
-              loss='binary_crossentropy', 
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
+              loss='binary_crossentropy',
               metrics=['accuracy'])
 
-early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
-lr_scheduler = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, verbose=1)
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.1, patience=3, restore_best_weights=True)
+#lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, verbose=1)
 
-history = model.fit(X_train, y_train, epochs=20, batch_size=32, validation_split=0.15, callbacks=[early_stopping, lr_scheduler])
+history = model.fit(X_train, y_train, epochs=20, batch_size=32, validation_split=0.15, callbacks=[early_stopping])
 
 loss, accuracy = model.evaluate(X_test, y_test)
 print(f'Accuracy: {accuracy}')
--- app/tests/test_create_embedding.py
+++ app/tests/test_create_embedding.py
@@ -1,14 +1,26 @@
+import numpy as np
+
 from ..dependencies.create_embeddings import create_embedding
 
 
 def test_create_embedding():
-    text1 = ["This is an example sentence"]
-    text2 = ["This is another sentence"]
+    text = ["This is an example sentence"]
     
-    embedding1 = create_embedding(text1)
-    embedding2 = create_embedding(text2)
+    embedding = create_embedding(text)
+
+    assert isinstance(embedding, np.ndarray)
+    assert len(embedding)==1
+    assert isinstance(embedding[0], np.ndarray)
+    assert len(embedding[0])==1024
+    assert isinstance(embedding[0][0], np.float32)
     
+def test_create_embedding_multiple():
+    text = ["This is an example sentence", "This is another example sentence"]
     
-    assert embedding1 != embedding2
-    
-   
+    embedding = create_embedding(text)
+
+    assert isinstance(embedding, np.ndarray)
+    assert len(embedding)==2
+    assert isinstance(embedding[0], np.ndarray)
+    assert len(embedding[0])==1024
+    assert isinstance(embedding[0][0], np.float32)
