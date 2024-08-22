dece942e0df3c69a086bd5c3b4b667c0efc523c7
Aziz Harun Çetinkaya <162803767+Haruncetinky@users.noreply.github.com>
2024-08-22 10:53:58 +0300

    Update test_create_embedding.py

--- app/tests/test_create_embedding.py
+++ app/tests/test_create_embedding.py
@@ -2,8 +2,8 @@ from ..dependencies.create_embeddings import create_embedding
 
 
 def test_create_embedding():
-    text1 = "This is an example sentence"
-    text2 = "This is another sentence"
+    text1 = ["This is an example sentence"]
+    text2 = ["This is another sentence"]
     
     embedding1 = create_embedding(text1)
     embedding2 = create_embedding(text2)
