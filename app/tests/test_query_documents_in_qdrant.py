319577aefdaf44ab4aa92a24aced29252bd82f73
Aziz Harun Çetinkaya <162803767+Haruncetinky@users.noreply.github.com>
2024-08-22 10:56:51 +0300

    Update test_query_documents_in_qdrant.py

--- app/tests/test_query_documents_in_qdrant.py
+++ app/tests/test_query_documents_in_qdrant.py
@@ -2,8 +2,10 @@ from ..dependencies.upload_documents import query_documents_in_qdrant
 
 
 def test_query_documents_in_qdrant():
-    results = query_documents_in_qdrant("python", 3)
-    assert len(results) == 3
+    query_text = "test query"
+    top_k = 3
+    results = query_documents_in_qdrant(query_text, top_k)
+    assert len(results) != 0
 
 def test_not_found_query():
     results = query_documents_in_qdrant("nonexistent_word", 5)
