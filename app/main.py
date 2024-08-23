bb402133ecbd5a75eeb21d9de21dea973d8ad0e0
Aziz Harun Çetinkaya <162803767+Haruncetinky@users.noreply.github.com>
2024-08-23 15:43:32 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -25,7 +25,15 @@ def query_documents(query: Query):
         return results
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
+        
+@app.post("/prediction/")
+def prediction(query: Query):
+    try:
+        results = result_prediction(query.query_text)
+        return results
+    except Exception as e:
+        raise HTTPException(status_code=500 , detail=str(e))
+        
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
